767c3f3086dd710a5b7671620290b019c1ae6c5e
zh-ao-ch-en <2434417767@qq.com>
2025-05-16 14:53:45 +0800

    Original Version

--- src/GamePanel/Main.java
+++ src/GamePanel/Main.java
@@ -10,6 +10,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         frame.pack();
+        frame.setResizable(false); // 禁止调整大小（包括最大化）
         frame.setLocationRelativeTo(null); // 居中
         frame.setVisible(true);
     }
